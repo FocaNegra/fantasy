6cde9b654a32002c5925a82bf391bb8b9a38650c
FocaNegra <victor.rn96@gmail.com>
2022-06-03 18:53:13 +0200

    solved SECRET_KEY issue

--- fantasy/settings/base.py
+++ fantasy/settings/base.py
@@ -19,13 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--3$vgp=#5k&w0q2x3mkmt6s(5gbd9n28^232nf4n1n)1b9qdew'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
