14ffbf375f5fd511e2e16f4fadf470944d0d88a3
FocaNegra <victor.rn96@gmail.com>
2022-06-04 19:03:39 +0200

    Login added

--- base/views.py
+++ base/views.py
@@ -1,9 +1,39 @@
 from re import I
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import League
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+def loginPage(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "Usuario no existe")
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Usuario o contraseña incorrectos")
+
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+def LogoutUser(request):
+    logout(request)
+    return redirect('home')
 
 
 def home(request):
