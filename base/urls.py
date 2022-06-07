b56e9c1e048b0c3e0228f1bd507b9d74c44b22a1
FocaNegra <victor.rn96@gmail.com>
2022-06-07 13:49:49 +0200

    add register option

--- base/urls.py
+++ base/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.loginPage, name="login"),
     path('logout/', views.LogoutUser, name="logout"),
+    path('register/', views.registerPage, name="register"),
     path('', views.home, name="home"),
     path('league/<str:pk>/', views.league, name="league"),
 ]
